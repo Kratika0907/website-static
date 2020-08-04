c2a72ba178f5c186a6abae22a538f9dc4fed2f23
Kratika0907 <kratika0907@gmail.com>
2020-08-04 23:01:17 +0530

    c2

--- scripts/checks.js
+++ scripts/checks.js
@@ -1,11 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    if (github.context.eventName === 'pull_request') {
-        const pushPayload = github.context.payload
-        console.log(pushPayload)
-} 
-} catch (error) {
-  core.setFailed(error.message);
+
+async function run() {
+    try {
+        if (github.context.eventName === 'push') {
+            const pushPayload = github.context.payload
+            core.info(pushPayload)
+    } 
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+
 }
+
+run();
+
