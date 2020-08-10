76442cd087158f9d449134e9e45371fb8ab7be62
Kratika0907 <kratika0907@gmail.com>
2020-08-10 22:45:12 +0530

    workflow 10

--- scripts/checks.js
+++ scripts/checks.js
@@ -5,59 +5,60 @@ const github = require('@actions/github');
 
 async function run() {
     try {
-        if (github.context.eventName === 'pull_request') {
-            const pullRequestPayload = github.context.payload
-            const {number : prNumber ,  action : prAction} = pullRequestPayload;
-            const myToken = core.getInput('myToken');
-            const octokitConfig = {
-                auth : myToken,
-                userAgent : 'rds-website-static v1.0.0',
-                baseUrl : 'https://api.github.com',
-                log: {
-                    debug: () => {},
-                    info: () => {},
-                    warn: console.warn,
-                    error: console.error
-                  },
-            }
-            const octokit = github.getOctokit(octokitConfig)
-            let pullRequestConfig = {
-                owner : 'Real-Dev-Squad',
-                repo : 'website-static',
-                pull_number: prNumber,
-                mediaType: {
-                    format: "diff",
-                  },
-            }
-            const {data : prMetaData} = await octokit.pulls.get({owner : 'Real-Dev-Squad',
-                                                                repo : 'website-static',
-                                                                pull_number: prNumber,});
-            const {data : prData} = await octokit.pulls.get(pullRequestConfig);
-            const extractDataFlag = await checkData(prData);
-            if (extractDataFlag && prMetaData.mergeable) {
-                // merge the PR 
-                const mergeConfig = {
-                    commit_title : 'Details for a new user',
-                    commit_message : `Details added for user ${prMetaData.head.user.login}`,
-                    sha : prMetaData.head.sha,
-                    merge_method : 'rebase'
-                }
-                const  mergePRStatus = await octokit.pulls.put(mergeConfig);
-                if (mergePRStatus.Headers.status === 200) {
-                    console.log('PR successfully merged')
-                }
-            }
-            else {
-                // comment and revert back // create a comment on issue the number is same as pr number 
-                const prCommentConfig = {
-                    owner : 'Real-Dev-Squad',
-                    repo : 'website-static',
-                    issues_number: prNumber,
-                    body : 'This PR has certain issues and cannot be merged please check readme and faq and resubmit'
-                }
-                const commentPR = await octokit.issues.createComment(prCommentConfig);
-                console.log(commentPR);
-            }
+        if (github.context.eventName === 'push') {
+            const payload = JSON.stringify(github.context.payload, undefined, 2)
+            console.log(payload)
+            // const {number : prNumber ,  action : prAction} = pullRequestPayload;
+            // const myToken = core.getInput('myToken');
+            // const octokitConfig = {
+            //     auth : myToken,
+            //     userAgent : 'rds-website-static v1.0.0',
+            //     baseUrl : 'https://api.github.com',
+            //     log: {
+            //         debug: () => {},
+            //         info: () => {},
+            //         warn: console.warn,
+            //         error: console.error
+            //       },
+            // }
+            // const octokit = github.getOctokit(octokitConfig)
+            // let pullRequestConfig = {
+            //     owner : 'Real-Dev-Squad',
+            //     repo : 'website-static',
+            //     pull_number: prNumber,
+            //     mediaType: {
+            //         format: "diff",
+            //       },
+            // }
+            // const {data : prMetaData} = await octokit.pulls.get({owner : 'Real-Dev-Squad',
+            //                                                     repo : 'website-static',
+            //                                                     pull_number: prNumber,});
+            // const {data : prData} = await octokit.pulls.get(pullRequestConfig);
+            // const extractDataFlag = await checkData(prData);
+            // if (extractDataFlag && prMetaData.mergeable) {
+            //     // merge the PR 
+            //     const mergeConfig = {
+            //         commit_title : 'Details for a new user',
+            //         commit_message : `Details added for user ${prMetaData.head.user.login}`,
+            //         sha : prMetaData.head.sha,
+            //         merge_method : 'rebase'
+            //     }
+            //     const  mergePRStatus = await octokit.pulls.put(mergeConfig);
+            //     if (mergePRStatus.Headers.status === 200) {
+            //         console.log('PR successfully merged')
+            //     }
+            // }
+            // else {
+            //     // comment and revert back // create a comment on issue the number is same as pr number 
+            //     const prCommentConfig = {
+            //         owner : 'Real-Dev-Squad',
+            //         repo : 'website-static',
+            //         issues_number: prNumber,
+            //         body : 'This PR has certain issues and cannot be merged please check readme and faq and resubmit'
+            //     }
+            //     const commentPR = await octokit.issues.createComment(prCommentConfig);
+            //     console.log(commentPR);
+            // }
 
 
             
